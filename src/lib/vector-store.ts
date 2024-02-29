d31030c454d208a6f6a67076617a8e34a3b9f04e
Yashmalbhage01 <87193552+Yashmalbhage01@users.noreply.github.com>
2024-02-29 19:45:09 +0530

    first commit

---
+++ src/lib/vector-store.ts
@@ -0,0 +1,45 @@
+import { env } from "./config";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+
+export async function embedAndStoreDocs(
+  client: PineconeClient,
+  // @ts-ignore docs type error
+  docs: Document<Record<string, any>>[]
+) {
+  /*create and store the embeddings in the vectorStore*/
+  try {
+    const embeddings = new OpenAIEmbeddings();
+    const index = client.Index(env.PINECONE_INDEX_NAME);
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: env.PINECONE_NAME_SPACE,
+      textKey: "text",
+    });
+  } catch (error) {
+    console.log("error ", error);
+    throw new Error("Failed to load your docs !");
+  }
+}
+
+// Returns vector-store handle to be used a retrievers on langchains
+export async function getVectorStore(client: PineconeClient) {
+  try {
+    const embeddings = new OpenAIEmbeddings();
+    const index = client.Index(env.PINECONE_INDEX_NAME);
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex: index,
+      textKey: "text",
+      namespace: env.PINECONE_NAME_SPACE,
+    });
+
+    return vectorStore;
+  } catch (error) {
+    console.log("error ", error);
+    throw new Error("Something went wrong while getting vector store !");
+  }
+}
